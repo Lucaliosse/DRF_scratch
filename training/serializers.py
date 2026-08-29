250e5c09d0e6603669e3b85bd36346e9078f1c64
Lucaliosse <tvergne.pro@gmail.com>
2026-08-29 12:01:37 +0200

    Mutualize Category serializers and add filed validations (unicity)

--- training/serializers.py
+++ training/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import (
+    ModelSerializer,
+    SerializerMethodField,
+    ValidationError,
+)
 
 from training.models import Category, Product
 
@@ -10,22 +14,34 @@ class ProductSerializer(ModelSerializer):
         fields = ["id", "name", "description", "active", "category"]
 
 
-class CategoryListSerializer(ModelSerializer):
-
+class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "date_created", "date_updated", "name", "products"]
 
+    def validate_name(self, value):
+        # Nous vérifions que la catégorie existe
+        if Category.objects.filter(name=value).exists():
+            # En cas d'erreur, DRF nous met à disposition l'exception ValidationError
+            raise ValidationError("Category already exists")
+        return value
 
-class CategoryDetailSerializer(ModelSerializer):
+    def validate(self, data):
+        pass
+        # if data['name'] not in data['description']:
+        #     raise ValidationError('Name must be in description')
+        # return data
+
+
+class CategoryListSerializer(CategorySerializer):
+    pass
+
+
+class CategoryDetailSerializer(CategorySerializer):
 
     # By using `SerializerMethodField', we need to implement a
     # 'get_XXX' where XXX is the attribut hame, here 'products'
     products = SerializerMethodField()
-
-    class Meta:
-        model = Category
-        fields = ["id", "date_created", "date_updated", "name", "products"]
 
     def get_products(self, instance):
         queryset = instance.products.filter(active=True)
