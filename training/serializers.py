061afbcab3b302f41fbeaaa80bbec8027fa9e26d
Lucaliosse <tvergne.pro@gmail.com>
2026-08-29 11:01:35 +0200

    Add filter on nested serializer

--- training/serializers.py
+++ training/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from training.models import Category, Product
 
@@ -12,8 +12,15 @@ class ProductSerializer(ModelSerializer):
 
 class CategorySerializer(ModelSerializer):
 
-    products = ProductSerializer(many=True)
+    # By using `SerializerMethodField', we need to implement a
+    # 'get_XXX' where XXX is the attribut hame, here 'products'
+    products = SerializerMethodField()
 
     class Meta:
         model = Category
         fields = ["id", "date_created", "date_updated", "name", "products"]
+
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return serializer.data
